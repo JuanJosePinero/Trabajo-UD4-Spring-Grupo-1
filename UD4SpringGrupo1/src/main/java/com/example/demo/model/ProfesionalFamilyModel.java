a8181ace950e733e47294d6d217bdcef3827be85
alexanderPat0 <alex0rodriguez7fenix@gmail.com>
2023-11-22 14:05:59 +0100

    Quitado anotaciones en modelos

--- UD4SpringGrupo1/src/main/java/com/example/demo/model/ProfesionalFamilyModel.java
+++ UD4SpringGrupo1/src/main/java/com/example/demo/model/ProfesionalFamilyModel.java
@@ -1,14 +1,10 @@
 package com.example.demo.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 public class ProfesionalFamilyModel {
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+
 	private int id;
 	private String name;
 }
--- UD4SpringGrupo1/src/main/java/com/example/demo/model/ServicioModel.java
+++ UD4SpringGrupo1/src/main/java/com/example/demo/model/ServicioModel.java
@@ -6,19 +6,12 @@ import com.example.demo.entity.Business;
 import com.example.demo.entity.ProfesionalFamily;
 import com.example.demo.entity.Student;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 public class ServicioModel {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+
 	private long id;
 	
 	private String title;
@@ -29,16 +22,12 @@ public class ServicioModel {
 	
 	private Date happeningDate;
 
-	@ManyToOne
-	@JoinColumn(name = "studentId_fk")
+
 	private Student studentId;
-	
-	@ManyToOne
-	@JoinColumn(name = "businessId_fk")
+
 	private Business businessId;
 	
-	@ManyToOne
-	@JoinColumn(name = "profesionalFamilyId_fk")
+
 	private ProfesionalFamily familyId;
 	
 	private int valoration;
