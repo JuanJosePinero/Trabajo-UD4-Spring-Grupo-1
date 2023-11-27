3c9d10d1b5562c9468172f07066df0fcfeabcd09
SamuraiJuanjo <Juanjosepi.orellana19@gmail.com>
2023-11-27 17:49:25 +0100

    changed authorizeRequests to authorizeHttpRequests

--- UD4SpringGrupo1/src/main/java/com/example/demo/security/SecurityConfig.java
+++ UD4SpringGrupo1/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 	    http
-	        .authorizeRequests(authorizeRequests ->
+	        .authorizeHttpRequests(authorizeRequests ->
 	            authorizeRequests
 	                .requestMatchers("/", "/courses/**", "/imgs/**", "/auth/**", "/webjars/**", "/css/**", "/files/**"
 	                ).permitAll()
